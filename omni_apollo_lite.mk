7eedd359890f6c96a6f80f535bf1d0495b65c50e
vinixast <63014441+vinixast@users.noreply.github.com>
2020-04-15 19:25:41 -0300

    Update omni_apollo_lite.mk

--- omni_apollo_lite.mk
+++ omni_apollo_lite.mk
@@ -1,6 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+
 
 
 # Replace $$DEVICE$$ with your Device Name's Value.
@@ -12,6 +15,16 @@ PRODUCT_NAME := omni_apollo_lite
 PRODUCT_BRAND := vernee
 PRODUCT_MODEL := apollo_lite
 PRODUCT_MANUFACTURER := vernee
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+    
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="vernee/apollo_lite/k15tb_a:6.0/MRA58K/1493198123:user/test-keys" \
+    PRIVATE_BUILD_DESC="full_k15tb_a-user 6.0 MRA58K 1493198123 test-keys"
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
