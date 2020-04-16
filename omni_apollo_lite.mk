1efe76fdf401f2932c37c26ee7d792b7648f4f50
vinixast <63014441+vinixast@users.noreply.github.com>
2020-04-16 00:53:28 -0300

    Update omni_apollo_lite.mk

--- omni_apollo_lite.mk
+++ omni_apollo_lite.mk
@@ -22,17 +22,17 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    #$(LOCAL_PATH)/recovery/root/etc/recovery.fstab:root/etc/recovery.fstab \
-    #$(LOCAL_PATH)/recovery/root/sbin/permissive.sh:root/sbin/permissive.sh \
-    #$(LOCAL_PATH)/recovery/root/factory_init.project.rc:root/factory_init.project.rc \
-    #$(LOCAL_PATH)/recovery/root/factory_init.rc:root/factory_init.rc \
+    $(LOCAL_PATH)/recovery/root/etc/recovery.fstab:root/etc/recovery.fstab \
+    $(LOCAL_PATH)/recovery/root/sbin/permissive.sh:root/sbin/permissive.sh \
+    $(LOCAL_PATH)/recovery/root/factory_init.project.rc:root/factory_init.project.rc \
+    $(LOCAL_PATH)/recovery/root/factory_init.rc:root/factory_init.rc \
     $(LOCAL_PATH)/recovery/root/init.rc:root/init.rc \
     $(LOCAL_PATH)/recovery/root/init.recovery.mt6797.rc:root/init.recovery.mt6797.rc \
     $(LOCAL_PATH)/recovery/root/init.recovery.usb.rc:root/init.recovery.usb.rc \
     $(LOCAL_PATH)/recovery/root/init.recovery.service.rc:root/init.recovery.service.rc \
-    #$(LOCAL_PATH)/recovery/root/meta_init.modem.rc:root/meta_init.modem.rc \
-    #$(LOCAL_PATH)/recovery/root/meta_init.project.rc:root/meta_init.project.rc \
-    #$(LOCAL_PATH)/recovery/root/meta_init.rc:root/meta_init.rcc \
-    #$(LOCAL_PATH)/recovery/root/ueventd.mt6797.rc:root/ueventd.mt6797.rc \
+    $(LOCAL_PATH)/recovery/root/meta_init.modem.rc:root/meta_init.modem.rc \
+    $(LOCAL_PATH)/recovery/root/meta_init.project.rc:root/meta_init.project.rc \
+    $(LOCAL_PATH)/recovery/root/meta_init.rc:root/meta_init.rcc \
+    $(LOCAL_PATH)/recovery/root/ueventd.mt6797.rc:root/ueventd.mt6797.rc \
     $(LOCAL_PATH)/recovery/root/fstab.mt6797:root/fstab.mt6797 \
     $(LOCAL_PATH)/recovery/root/ueventd.rc:root/ueventd.rc
