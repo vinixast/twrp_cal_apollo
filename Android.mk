2ac596fb147b1fdb8358eaaf32dc216b2821157e
vinixast <63014441+vinixast@users.noreply.github.com>
2020-06-01 17:30:56 -0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,6 +1,8 @@
+LOCAL_PATH := device/vernee/apollo_x
+
 ifneq ($(filter apollo_x,$(TARGET_DEVICE)),)
 
-LOCAL_PATH:=device/vernee/apollo_x,
+LOCAL_PATH := device/vernee/apollo_x
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
